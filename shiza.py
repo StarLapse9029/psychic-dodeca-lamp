74afe9c134a12c5c2db2e790b34b866229af6159
StarLapse9029 <kenzokurota23@gmail.com>
2026-06-19 14:02:55 -0300

    simple rotation cipher brute force

---
+++ shiza.py
@@ -0,0 +1,30 @@
+import sys
+
+def main():
+    if len(sys.argv) > 2:
+        print("Too many arguments!!!")
+        return
+
+    orig = sys.argv[1]
+    print("Original string: ")
+    for i in range(26):
+        print(rotate(orig, i))
+
+# A = 65 Z = 90
+# a = 97 z = 122
+def rotate(string, amount):
+    new_str = ""
+    for character in string:
+        i = ord(character)
+        if i > 64 and i < 91:
+            i += amount
+            if i > 90:
+                i -= 26
+        elif i > 96 and i < 123:
+            i += amount
+            if i > 122:
+                i -= 26
+        new_str += chr(i)
+    return new_str
+
+main()
